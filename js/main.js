06fa951eebfc69b308bf3030c3aa373fa2da7ac5
AidySun <aidyszh@gmail.com>
2016-12-19 16:24:35 +0800

    select2

--- js/main.js
+++ js/main.js
@@ -1,58 +1,90 @@
+var $select2Obj = $("select");
 
-$('#myselect').on('keydown',function(e) {
-	var key = e.which;
-  if(key == 13) { // the enter key code
-    //doAction($("#myselect").val());
-    //window.close();
-  } 
-  if(key == 27) { // the escape key code
-    window.close();
-  }
+
+function redirect() {
+  new CSInterface().evalScript("$._Ext_IDSN.doesFileExist(\"" + getDataFile() + "\")", function(result) {
+   if (result == "Yes") {
+     window.location=getDataFile();
+   }
+  });
+}
+
+function onLoad() {
+  new CSInterface().evalScript("$._Ext_IDSN.doesFileExist(\"" + getDataFile() + "\")", function(result) {
+    if (result == "No") {
+      createHTML();
+      location.reload();
+    }
+  });
+
+  $select2Obj.val("").trigger("change");
+  $select2Obj.select2("open");
+}
+
+$select2Obj.on("select2:select", function (e) { 
+//  smallUI();
+  var value = $("#menuList").val();
+  doAction(value);
+  window.close();
 });
+
+$select2Obj.on("select2:opening", function (e) { 
+  CSInterface.prototype.resizeContent(615, 450);
+  $select2Obj.val(null).trigger("change");
+});
+
+$select2Obj.on("select2:open", function (e) { 
+  $select2Obj.val(null).trigger("change");
+});
+
+$select2Obj.on("select2:closing", function (e) { 
+  smallUI();
+});
+
+$select2Obj.on("select2:close", function (e) { 
+  window.close();
+});
+
 
 $('#refresh').on('click', function(e) {
-  createHTML();
+//  createHTML();
+location.reload();
 });
 
+
 function doAction(actionID) {
-	var script = "app.menuActions.itemByID(" + actionID + ").invoke()";
-	var csInterface = new CSInterface();
-	csInterface.evalScript(script);
+  var script = "app.menuActions.itemByID(" + actionID + ").invoke()";
+  var csInterface = new CSInterface();
+  csInterface.evalScript(script);
 }
 
-function myOnLoad() {
-    new CSInterface().evalScript("$._Ext_IDSN.doesFileExist(\"" + tempJSFFilePath() + "\")", function (result) {
-      if (result == "No") {
-        createHTML();
-        location.reload();
-      }
-    }); 
+function smallUI() {
+  CSInterface.prototype.resizeContent(615, 60);
 }
 
-function tempJSFFilePath() { 
-  return createTempFolder() + "IDSN.html.js";
+function getDataFile() {
+  var sysPath = new CSInterface().getSystemPath(SystemPath.EXTENSION);
+  var hostEnv = new CSInterface().getHostEnvironment();
+  var dataFile = sysPath + '/' + hostEnv.appId + '_' + hostEnv.appUILocale + '_' + hostEnv.appVersion + '.js';
+  return dataFile;
 }
 
 function createHTML() {
-	var tempFile = tempJSFFilePath();
-	new CSInterface().evalScript("$._Ext_IDSN.selectWithOptions()", function (result) {
-		window.cep.fs.writeFile(tempFile, result);
-		});	
+  var tempFile = getDataFile();
+  new CSInterface().evalScript("$._Ext_IDSN.selectWithOptions()", function(result) {
+    window.cep.fs.writeFile(tempFile, result);
+  });
 }
 
-function createTempFolder() {
-  var tempFolder = getTempFolder();
-  window.cep.fs.makedir(tempFolder);
-  //window.cep.fs.setPosixPermissions(tempFolder, "777");
-  return tempFolder;
-}
-
-function getTempFolder() {
-  var tempFolderName = 'com.example.helloworld.extension/';
-  var tempFolder = '/tmp/' + tempFolderName;
-  if (window.navigator.platform.toLowerCase().indexOf('win') > -1) {
-    tempFolder = new CSInterface().getSystemPath(SystemPath.USER_DATA) + '/../Local/Temp/' + tempFolderName;
+$('input').on('keydown', function(e) {
+  var key = e.which;
+  //if (key == 13) { // the enter key code
+    //doAction($("#menuList").val());
+    //window.close();
+  //}
+  if (key == 27) { // the escape key code
+    //$('#menuList').select2().select2('close');
+    $select2Obj.select2("close");
+    window.close();
   }
-  return tempFolder;
-}
-
+});
---
+++ js/select2.keydown.js
@@ -0,0 +1,104 @@
+// http://codepen.io/jessehouchins/pen/rWZxwr
+// https://github.com/select2/select2/issues/3279
+
+function regexpStr(str) {
+  var specials = ["/", ".", "*", "+", "?", "|", "(", ")", "[", "]", "{", "}", "\\", "^", "$"];
+  var regexp = new RegExp("(\\" + specials.join("|\\") + ")", "g");
+  str = str.replace(regexp, "\\$1"); // replace symbol
+  str = str.replace(/^\s+|\s+$/g, "").replace(/\s+/g, " "); // trim begin, end and between words
+  // 'and' condition
+  str = str.split(" ").join(")(?=.*");
+  str = "^(?=.*" + str + ").*$";
+  return str;
+}
+
+function matchAny(term, text) {
+  search = new RegExp(regexpStr(term), "i");
+  if (search.test(text)) {
+    return true;
+  }
+  return false;
+}
+
+  $.fn.select2.amd.require([
+    'select2/defaults',
+    'select2/keys',
+    'select2/utils',
+    'select2/compat/matcher'
+  ], function(Defaults, KEYS, Utils, oldMatcher) {
+    function SearchWhenClosed() {}
+
+    SearchWhenClosed.prototype.bind = function(decorated, container, $container) {
+      var self = this
+      decorated.call(this, container, $container)
+
+      container.$selection.on('keydown', function(evt) {
+        self._handleSearchWhenClosed(evt)
+      })
+    }
+
+    SearchWhenClosed.prototype._handleSearchWhenClosed = function(originalMethod, evt) {
+
+      // For now, exit unless we know search is going to be there
+      if (this.options.get('minimumResultsForSearch')) return
+
+      var self = this
+      var key = evt.which
+      var irrelevantKeys = [
+        8,  // DELETE
+        9,  // TAB
+        13, // ENTER
+        16, // SHIFT
+        17, // CTRL
+        18, // OPT/ALT
+        20, // CAPS
+        27, // ESC
+        32, // SPACE
+        37, // LEFT
+        38, // UP
+        39, // RIGHT
+        40, // DOWN
+        91, // L-CMD
+        93, // R-CMD
+      ]
+      var doOpen = (key === KEYS.ENTER || key === KEYS.SPACE || (key === KEYS.DOWN && evt.altKey))
+      var doSearch = !doOpen && !evt.altKey && irrelevantKeys.indexOf(key) === -1
+
+      if (doSearch || doOpen) {
+        self.trigger('open')
+        self.$search.focus().select()
+        // For some reason, the query doesn't always fire the first time
+        setTimeout(function() {
+          self.trigger('query', {
+            term: self.$search.val()
+          })
+        })
+      } else if (doOpen) {
+        self.trigger('open')
+        evt.preventDefault()
+      }
+
+    }
+
+    var _apply = Defaults.constructor.prototype.apply
+    Defaults.constructor.prototype.apply = function() {
+      var options = _apply.apply(this, arguments)
+      if (options.minimumResultsForSearch === 0 && !options.multiple) {
+        options.dropdownAdapter = Utils.Decorate(
+          options.dropdownAdapter,
+          SearchWhenClosed
+        )
+      }
+      return options
+    }
+    
+    
+    $('select').select2({
+      // maximumSelectionLength: 1,
+      selectOnClose: false,
+      closeOnSelect: true,
+      multiple: 'multiple',
+      placeholder: 'Search Menus (ENTER to run, ESC to close)',
+      matcher: oldMatcher(matchAny)
+    });
+  });
